64b710a6f33cd523d76e2c4a27b74d0ca0e866cc
Yassa Siddiqui <siddiquiyassa@gmail.com>
2022-03-09 00:24:50 +0000

    front end added but still not working

---
+++ RBO_App/rbo-frontend-react/src/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AuthService from "./services/auth.service";
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+// import BoardEmployee from "./components/board-moderator.component";
+// import BoardAdmin from "./components/board-admin.component";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+    this.state = {
+      showEmployeeBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showEmployeerBoard: user.roles.includes("ROLE_EMPLOYEE"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+  logOut() {
+    AuthService.logout();
+  }
+  render() {
+    const { currentUser, showEmployeeBoard, showAdminBoard } = this.state;
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            ReadBooks Online
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            {showEmployeeBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Employee Board
+                </Link>
+              </li>
+            )}
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+        <div className="container mt-3">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/login" element={<Login/>} />
+            <Route exact path="/register" element={<Register/>} />
+            <Route exact path="/profile" element={<Profile/>} />
+            <Route exact path="/user" element={<BoardUser/>} />
+            {/* <Route path="/mod" element={BoardEmployee} />
+            <Route path="/admin" element={BoardAdmin} /> */}
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
