19666e72f48b56f99708bf0d1ed84d69fdbd7208
Yassa Siddiqui <siddiquiyassa@gmail.com>
2022-03-16 17:41:45 +0000

    finale commit before submission

--- RBO_App/rbo-frontend-react/src/components/board-employee.component.js
+++ RBO_App/rbo-frontend-react/src/components/board-employee.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import UserService from "../services/user.service";
+import PurchaserequestList from "../components/purchaserequest-list.component";
+
 //employee view
 export default class BoardEmployee extends Component {
   constructor(props) {
@@ -36,6 +38,20 @@ export default class BoardEmployee extends Component {
         <header className="jumbotron">
           <h3>{this.state.content}</h3>
         </header>
+        <PurchaserequestList />
+        <div className="list row">
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllPurchaseRequests}
+          >
+            Remove All
+          </button>
+          <button
+            className="m-3 btn btn-sm btn-success"
+          >
+            Allocate All
+          </button>
+          </div>
       </div>
     );
   }
